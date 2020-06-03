e7512aa0eab2e2bd23b447293b0cf720eb850377
Ben P <ben.h.poh@gmail.com>
2020-06-03 17:47:39 +1000

    Heroku deploy bug fix

--- models/planets.rb
+++ models/planets.rb
@@ -1,7 +1,7 @@
 require "pg"
 
 def run_sql(sql)
-    conn = PG.connect(ENV['DATABASE_URL'] || dbname: 'galaxy')
+    conn = PG.connect(ENV['DATABASE_URL'] || {dbname: 'galaxy'})
     records = conn.exec(sql)
     conn.close
     records
